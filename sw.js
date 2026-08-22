73fd7bc921deac6ed6f2c484bd3ea62fd7fa5529
phatcharanun <153785070+phatcharanun@users.noreply.github.com>
2026-08-22 21:52:10 +0700

    add icon and sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'camera-gps-app-v1';
+const APP_SHELL = [
+    './',
+    './index.html',
+    './manifest.json',
+    './CSS/style.css',
+    './js/camera.js',
+    './js/location.js',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
